12119e7dadc61d7a6eb470ead08ca8793e34dcfb
Kogi1337 <alexkogler0@gmail.com>
2021-03-04 23:02:18 +0100

    Add - viewcomponents for the project

--- app/src/main/java/com/kogler/se2einzelprojekt/MainActivity.java
+++ app/src/main/java/com/kogler/se2einzelprojekt/MainActivity.java
@@ -3,12 +3,26 @@ package com.kogler.se2einzelprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtResponse;
+    private Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //komponenten die benutzt werden initialisieren
+        txtResponse = (TextView) findViewById(R.id.txtAnswer);
+        btnSend = (Button) findViewById(R.id.btnSend);
+
+        //Onclick action setzen
+        btnSend.setOnClickListener(v -> {
+            //Do something
+        });
     }
 }
